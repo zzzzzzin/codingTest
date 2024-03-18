ba5250bb8230728042f43e1f5538af8b3d24cd56
zzzzzzin <jyj7314@naver.com>
2024-03-18 17:50:06 +0900

    1912번 풀이 완료

--- JavaAlgorithm/src/bruteForceSearch/Q1912.java
+++ JavaAlgorithm/src/bruteForceSearch/Q1912.java
@@ -3,32 +3,30 @@ package bruteForceSearch;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Random;
 
 public class Q1912 {
 	public static int n; //입력할 정수의 갯수
-	public static int max = 0; //결과
-	
+	public static int maxSum = 0; //최댓값
+	public static int currentSum = 0; //연속하는 수의 합
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		ArrayList<Integer> list = new ArrayList<>();
-		
+
+		int n= Integer.parseInt(br.readLine());
+		String[] input = br.readLine().split(" ");
 		Random r = new Random();
 		
-		n = Integer.parseInt(br.readLine());
-		
+		int[] arr = new int[n];
 		for (int i=0; i<n; i++) {
-			System.out.print(r.nextInt(2001)-1000+" ");
-			list.add(r.nextInt(2001)-1000);
+			arr[i] = Integer.parseInt(input[i]);
 		}
 		
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<i; j++) {
-				
-			}
+		for (int i=1; i<n; i++) {
+			currentSum = Math.max(arr[i], currentSum + arr[i]);
+			
+			maxSum = Math.max(maxSum, currentSum); 
 		}
-		
+		System.out.println(maxSum);
 	}
 }
